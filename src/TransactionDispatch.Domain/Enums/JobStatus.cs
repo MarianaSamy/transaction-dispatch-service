3c175beb3ba02100c584cef8d30268406e098198
MarianaSamy <mariana_nagy@hotmail.com>
2025-11-08 10:41:14 +0200

    feat(application): add job status enum, and domain reference to the application layer

---
+++ src/TransactionDispatch.Domain/Enums/JobStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace TransactionDispatch.Domain.Enums
+{
+    public enum JobStatus
+    {
+        Pending = 0,
+        InProgress = 1,
+        Completed = 2,
+        Failed = 3,
+        Cancelled = 4
+    }
+}
